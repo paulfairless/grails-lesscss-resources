4a5c5e4c4313edf8cd98995b3cc695f787093397
Paul Fairless <paul.fairless@gmail.com>
2011-03-19 21:11:19 +0000

    removed unused methods

--- LesscssResourcesGrailsPlugin.groovy
+++ LesscssResourcesGrailsPlugin.groovy
@@ -21,31 +21,4 @@ This plugin if to support server-side compliation of .less CSS files to their .c
 
     // URL to the plugin's documentation
     def documentation = "https://github.com/paulfairless/grails-lesscss-resources"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
